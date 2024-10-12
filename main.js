1accac4759d832a585598d76cad4159956dbc565
Gustavo Kruger <gusdkruger@gmail.com>
2024-10-12 14:40:10 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("qr-form");
+form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const text = document.getElementById("input-field").value.trim();
+    const button = document.getElementById("button");
+    const container = document.getElementById("qr-container");
+    const qrCode = document.getElementById("qr-code");
+    button.innerText = "Generating..."
+    qrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${text}`;
+
+    qrCode.addEventListener("load",() => {
+        container.classList.add("generated");
+        button.innerText = "Generated!";
+    })
+});
