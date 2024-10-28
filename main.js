773e69ab9c058d08ae8ef34d1f84a45510f3e1cb
Gustavo Kruger <gusdkruger@gmail.com>
2024-10-27 23:07:56 -0300

    Improved css and js

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 const form = document.getElementById("qr-form");
-form.addEventListener("submit", event => {
+const button = document.getElementById("qr-form-btn");
+
+form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const text = document.getElementById("input-field").value.trim();
-    const button = document.getElementById("button");
+    const text = document.getElementById("qr-input-field").value.trim();
     const container = document.getElementById("qr-container");
     const qrCode = document.getElementById("qr-code");
-    button.innerText = "Generating..."
+    button.innerText = "Generating...";
     qrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${text}`;
 
-    qrCode.addEventListener("load",() => {
+    qrCode.addEventListener("load", () => {
         container.classList.add("fade-in");
         button.innerText = "Generated!";
-        setTimeout(() => {
-            button.innerText = "Generate";
-        }, 3500);
     })
+});
+
+button.addEventListener("mouseover", () => {
+    if(button.innerText === "Generated!") {
+        button.innerText = "Generate New!";
+    }
+});
+button.addEventListener("mouseout", () => {
+    if(button.innerText === "Generate New!") {
+        button.innerText = "Generated!";
+    }
 });
