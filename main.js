05b44bcca3e1f94f58a4158353430529c9733f5f
Gustavo Kruger <gusdkruger@gmail.com>
2024-11-02 21:12:47 -0300

    Minor improvements

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
-const form = document.getElementById("qr-form");
-const button = document.getElementById("qr-form-btn");
+const form = document.getElementById("form");
+const input = document.getElementById("form__input");
+const button = document.getElementById("form__submit");
+const container = document.getElementById("container");
+const qrCode = document.getElementById("container__qr-code");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-
-    const text = document.getElementById("qr-input-field").value.trim();
-    const container = document.getElementById("qr-container");
-    const qrCode = document.getElementById("qr-code");
-    button.innerText = "Generating...";
-    qrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${text}`;
-
-    qrCode.addEventListener("load", () => {
-        container.classList.add("fade-in");
-        button.innerText = "Generated!";
-    })
+    const text = input.value.trim();
+    if(text) {
+        button.innerText = "Generating...";
+        qrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${text}`;
+    }
+    else {
+        alert("Input text must not be empty!");
+        input.value = "";
+    }
 });
 
 button.addEventListener("mouseover", () => {
-    if(button.innerText === "Generated!") {
+    if(button.innerText == "Generated!") {
         button.innerText = "Generate New!";
     }
 });
+
 button.addEventListener("mouseout", () => {
-    if(button.innerText === "Generate New!") {
+    if(button.innerText == "Generate New!") {
         button.innerText = "Generated!";
     }
 });
+
+qrCode.addEventListener("load", () => {
+    container.classList.add("fade-in");
+    button.innerText = "Generated!";
+})
